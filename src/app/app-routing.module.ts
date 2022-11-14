7d0cb8412ef4351a550d2fd4403137940e252545
Mohsen <mohsen77sk@gmail.com>
2022-11-14 15:25:51 +0330

    Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout';
 
 const routes: Routes = [
-  // Redirect empty path to '/resume'
-  { path: '', pathMatch: 'full', redirectTo: 'resume' },
+  // Redirect empty path to '/'
+  { path: '', pathMatch: 'full', redirectTo: '' },
   {
     path: '',
     component: LayoutComponent,
     children: [
       {
-        path: 'resume',
+        path: '',
         loadChildren: () =>
           import('./modules/resume/resume.module').then((m) => m.ResumeModule),
       },
